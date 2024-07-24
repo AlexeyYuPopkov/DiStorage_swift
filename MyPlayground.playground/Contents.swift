b11878ad076c0bd132b9373c416c23a1d84b59a2
Alexey Popkov <alexey.popkov@pelc-consult.com>
2024-07-24 12:21:31 +0300

    example: project structure - create folders instead groups

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+for i in [0, 1, 2, 3] {
+    print("i is \(i)")
+}
+
+//for i in [0,1,2,3] {
+//    print(i.formatted())
+////    print(((i+1) % 4).formatted())
+//}
+print(0 % 4)
+print(1 % 4)
+print(2 % 4)
+print(3 % 4)
